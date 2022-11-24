ca9e14f3ca0dde560a9138e2b75e1a74baeb85b4
genzo61 <101041208+genzo61@users.noreply.github.com>
2022-11-24 12:14:57 +0300

    C#
    with sql database just importatnt command

---
+++ ilişkilitablolarkullanma/ilişkilitablolarkullanma/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ilişkilitablolarkullanma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dbilişkiliveritabanlıhareketDataSet = new ilişkilitablolarkullanma.DbilişkiliveritabanlıhareketDataSet();
+            this.proje6BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.proje6TableAdapter = new ilişkilitablolarkullanma.DbilişkiliveritabanlıhareketDataSetTableAdapters.proje6TableAdapter();
+            this.ürünAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.müşteriDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.personelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fiyatDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbilişkiliveritabanlıhareketDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proje6BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ürünAdDataGridViewTextBoxColumn,
+            this.müşteriDataGridViewTextBoxColumn,
+            this.personelDataGridViewTextBoxColumn,
+            this.fiyatDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.proje6BindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 433);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dbilişkiliveritabanlıhareketDataSet
+            // 
+            this.dbilişkiliveritabanlıhareketDataSet.DataSetName = "DbilişkiliveritabanlıhareketDataSet";
+            this.dbilişkiliveritabanlıhareketDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // proje6BindingSource
+            // 
+            this.proje6BindingSource.DataMember = "proje6";
+            this.proje6BindingSource.DataSource = this.dbilişkiliveritabanlıhareketDataSet;
+            // 
+            // proje6TableAdapter
+            // 
+            this.proje6TableAdapter.ClearBeforeFill = true;
+            // 
+            // ürünAdDataGridViewTextBoxColumn
+            // 
+            this.ürünAdDataGridViewTextBoxColumn.DataPropertyName = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.HeaderText = "Ürün Ad";
+            this.ürünAdDataGridViewTextBoxColumn.Name = "ürünAdDataGridViewTextBoxColumn";
+            // 
+            // müşteriDataGridViewTextBoxColumn
+            // 
+            this.müşteriDataGridViewTextBoxColumn.DataPropertyName = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.HeaderText = "Müşteri";
+            this.müşteriDataGridViewTextBoxColumn.Name = "müşteriDataGridViewTextBoxColumn";
+            // 
+            // personelDataGridViewTextBoxColumn
+            // 
+            this.personelDataGridViewTextBoxColumn.DataPropertyName = "Personel";
+            this.personelDataGridViewTextBoxColumn.HeaderText = "Personel";
+            this.personelDataGridViewTextBoxColumn.Name = "personelDataGridViewTextBoxColumn";
+            // 
+            // fiyatDataGridViewTextBoxColumn
+            // 
+            this.fiyatDataGridViewTextBoxColumn.DataPropertyName = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.HeaderText = "Fiyat";
+            this.fiyatDataGridViewTextBoxColumn.Name = "fiyatDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(764, 433);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("MV Boli", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbilişkiliveritabanlıhareketDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proje6BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DbilişkiliveritabanlıhareketDataSet dbilişkiliveritabanlıhareketDataSet;
+        private System.Windows.Forms.BindingSource proje6BindingSource;
+        private DbilişkiliveritabanlıhareketDataSetTableAdapters.proje6TableAdapter proje6TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ürünAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn müşteriDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn personelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fiyatDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ ilişkilitablolarkullanma/ilişkilitablolarkullanma/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient; 
+
+namespace ilişkilitablolarkullanma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'dbilişkiliveritabanlıhareketDataSet.proje6' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.proje6TableAdapter.Fill(this.dbilişkiliveritabanlıhareketDataSet.proje6);
+            // TODO: Bu kod satırı 'dbilişkiliveritabanlıhareketDataSet.proje6' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.proje6TableAdapter.Fill(this.dbilişkiliveritabanlıhareketDataSet.proje6);
+
+        }
+    }
+}
